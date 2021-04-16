263bab67fd6b37561b55c0e6d30dd0f0348199f8
wang-ax <3071254175@qq.com>
2021-04-16 22:57:45 +0800

    Junit5新特性练习

---
+++ Junit框架学习/test/java/JUnit5NewTests.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+
+/**
+ * ClassName PACKAGE_NAME
+ * Description TODO
+ * Author 30712
+ * Date 2021-04-15
+ * Time 21:51
+ */
+public class JUnit5NewTests {
+    @BeforeEach
+    @DisplayName("每条用例开始执行")
+    void start(){
+        
+    }
+}
